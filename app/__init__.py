9e11e36934f559bfd48b5cbd598efb2c169d03f6
User <kratden5@gmail.com>
2020-12-14 16:59:19 +0200

    Initial lab6 commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+import app.credentials
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + credentials.username + ':' + credentials.password + '@localhost:5432/budget_planner'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app.models import *
+from app.routes import *
